33d3f6cc93c13af58159d6b6f5c25ef3c0db8935
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 22:58:58 -0600

    Add author, article, and relationship unordered lists

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -5,9 +5,31 @@
 		<title> Conceptual Model</title>
 	</head>
 	<body>
-		<h1> Conceptual Model</h1>
+		<h1>Conceptual Model</h1>
+		<h2>Author</h2>
+			<ul>
+				<li>authorId (primary key)</li>
+				<li>authorProfile</li>
+				<li>authorName</li>
+				<li>authorEmail</li>
+				<li>authorTitle</li>
+				<li>authorTweetId</li>
+				<li>authorProfilePicture</li>
+			</ul>
+		<h2>Article</h2>
+			<ul>
+				<li>articleId (primary Key)</li>
+				<li>articleDate</li>
+				<li>articleTitle</li>
+			</ul>
+		<h2>Relationship</h2>
+			<ul>
+				<li>One Author can write many articles (1 to n)</li>
+				<li>One article can be written by one author (1 to 1)</li>
+			</ul>
 
 
+		<hr/>
 		<h2>Navigation</h2>
 		<ul>
 			<li><a href="./index.php">Return to Home</a></li>

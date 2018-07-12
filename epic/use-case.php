39108963e3ed1adc6b3bed5fa641ecf0819260b1
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 22:50:33 -0600

    Add use case information in ordered list format.

--- epic/use-case.php
+++ epic/use-case.php
@@ -6,8 +6,21 @@
 	</head>
 	<body>
 		<h1> Use Case</h1>
+		<ol>
+			<li>Tim writes a new article on Google's Selfish Ledger. Tim uses ArsTechnica/CondéNast's writing style guide and HTML tagging scheme that allows the default CSS formatting to format his article and pictures. Tim then logs into arstechnica.com/admin with his credentials.</li>
+			<li>The arstechnica site loads Tim's employee dashboard.</li>
+			<li>Tim Clicks on the "submit an article" button.</li>
+			<li>The website provides an interface for article photos to be uploaded. The first photo uploaded will be the leading image, like a craigslist ad.</li>
+			<li>Tim selects three images for the article from his hard drive. His leading photo is of a grown male in the fetal position under a desk with the remains of a destroyed chromebook and android phone  strewn about. Tim double checks that his image file names are the same that are referenced in his img tags, then he  clicks the upload button.</li>
+			<li>Next, the website displays an open textbox asking for HTML formatted text.</li>
+			<li>Tim copies the text from his word processor and pastes it into the textbox then clicks submit.</li>
+			<li>The website presents a preview of  what the article will look like. In an overlaid bar in the footer Tim is presented with two buttons, "edit article" and "submit for review."</li>
+			<li>Tim is satisfied with the layout of the page and clicks the "submit for review" button.</li>
+			<li>The website returns a confirmation and leaves Tim with the following message: "thank you for your article submittal. You will be notified when your supervisor has approved the article to be pushed to the arstechnica front page."</li>
+		</ol>
 
 
+		<hr/>
 		<h2>Navigation</h2>
 		<ul>
 			<li><a href="./index.php">Return to Home</a></li>

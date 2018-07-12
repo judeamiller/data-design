02fea38b2ede6e30d693e77cb002acfb1e8f9f10
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 20:41:34 -0600

    Added info for name, age, profession, attitude, and tech

--- epic/timothylee.php
+++ epic/timothylee.php
@@ -5,7 +5,16 @@
 		<title>Tim Lee's Persona</title>
 	</head>
 	<body>
-		<h1>Persona</h1>
+		<h1>Persona:</h1>
+		<img src="./timothylee.jpg" alt="photo of Tim"/>
+		<ul>
+			<li><strong>Name:</strong> Timothy Lee</li>
+			<li><strong>Age:</strong> 42</li>
+			<li><strong>Profession:</strong> Technology Writer for Ars Technica</li>
+			<li><strong>Attitude:</strong> Tim is a tech savvy writer for ArsTechnica. Though tim has a CS degree, his job is not writing code for the website, but rather articles on technology and society for ArsTechnica. </li>
+			<li><strong>Technology:</strong> Tim currently uses a Dell XPS laptop running Ubuntu 18.04, but is regularly testing out various  devices that he is reviewing for ArsTechnica. He may be using a MacBook Pro one week, and a Chromebook the next. Though Tim has a Pixel 2 XL, he never uses it for composing articles for work.</li>
+		</ul>
+		</ul>
 		<a href="./index.php">Return to Home</a>
 	</body>
 

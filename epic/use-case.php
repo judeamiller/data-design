7887c08c6afb8e8b5ab9742e56b94e96532ada0b
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 20:22:03 -0600

    Initial commit for use case.

---
+++ epic/use-case.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title> Use Case</title>
+	</head>
+	<body>
+		<h1> Use Case</h1>
+
+		<a href="./index.php">Return to Home</a>
+	</body>
+
+</html>

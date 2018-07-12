cef20f2dc4d881eafac9820fcdc4cc11df453706
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 20:21:38 -0600

    Initial commit for Conceptual model.

---
+++ epic/conceptual-model.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title> Conceptual Model</title>
+	</head>
+	<body>
+		<h1> Conceptual Model</h1>
+
+		<a href="./index.php">Return to Home</a>
+	</body>
+
+</html>

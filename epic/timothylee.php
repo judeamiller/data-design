4ada2fb760cf3133471c5e8242ebc352b6d70d14
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 20:48:35 -0600

    Added Gender, Added screen res and browser to tech section.

--- epic/timothylee.php
+++ epic/timothylee.php
@@ -10,9 +10,10 @@
 		<ul>
 			<li><strong>Name:</strong> Timothy Lee</li>
 			<li><strong>Age:</strong> 42</li>
+			<li><strong>Gender:</strong>Male</li>
 			<li><strong>Profession:</strong> Technology Writer for Ars Technica</li>
 			<li><strong>Attitude:</strong> Tim is a tech savvy writer for ArsTechnica. Though tim has a CS degree, his job is not writing code for the website, but rather articles on technology and society for ArsTechnica. </li>
-			<li><strong>Technology:</strong> Tim currently uses a Dell XPS laptop running Ubuntu 18.04, but is regularly testing out various  devices that he is reviewing for ArsTechnica. He may be using a MacBook Pro one week, and a Chromebook the next. Though Tim has a Pixel 2 XL, he never uses it for composing articles for work.</li>
+			<li><strong>Technology:</strong> Tim currently uses a Dell XPS laptop running Ubuntu 18.04 on a 3840 x 2160 display, but is regularly testing out various  devices that he is reviewing for ArsTechnica. He may be using a MacBook Pro one week, and a Chromebook the next. Though Tim has a Pixel 2 XL, he never uses it for composing articles for work. No matter which OS Tim uses, he only uses the latest FireFox browser for work.</li>
 		</ul>
 		</ul>
 		<a href="./index.php">Return to Home</a>

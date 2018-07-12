fac85a85030046083aa997299fadc6253f0eabeb
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 21:01:58 -0600

    Add Site Navigation

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -8,6 +8,12 @@
 		<h1> Conceptual Model</h1>
 
 		<a href="./index.php">Return to Home</a>
+		<h2>Navigation</h2>
+		<ul>
+			<li><a href="./index.php">Return to Home</a></li>
+			<li><a  href="./timothylee.php">Persona</a></li>
+			<li><a href="./use-case.php">Use Case</a></li>
+		</ul>
 	</body>
 
 </html>

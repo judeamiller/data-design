542e251cb80c9149c10bfa80a38cdf782874ba6e
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 20:17:45 -0600

    Inital Commit for Tim Lee's Persona Page.

---
+++ epic/timothylee.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Tim Lee's Persona</title>
+	</head>
+	<body>
+		<h1>Persona</h1>
+		<a href="./index.php">Return to Home</a>
+	</body>
+
+</html>

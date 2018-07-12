7a4ecb1ac1b1222cc40a934c4f5365c345e08054
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-11 21:03:26 -0600

    Add Site Navigation

--- epic/use-case.php
+++ epic/use-case.php
@@ -8,6 +8,12 @@
 		<h1> Use Case</h1>
 
 		<a href="./index.php">Return to Home</a>
+		<h2>Navigation</h2>
+		<ul>
+			<li><a href="./index.php">Return to Home</a></li>
+			<li><a  href="./conceptual-model.php">Conceptual Model</a></li>
+			<li><a href="./timothylee.php">Persona</a></li>
+		</ul>
 	</body>
 
 </html>

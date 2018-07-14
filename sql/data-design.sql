feee067cdd0bfbc48e254e79cffec0f6559ebda1
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-13 21:26:06 -0600

    Edit author and article tables. Checked for typos and ran commands to local database via MAMP to test code.

--- sql/data-design.sql
+++ sql/data-design.sql
@@ -1,5 +1,5 @@
---underscore change_me ??
-ALTER DATABASE data-design_CHANGE_ME CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+ALTER DATABASE data-design CHARACTER SET utf8 COLLATE utf8_unicode_ci;
 
 DROP TABLE IF EXISTS article;
 DROP TABLE IF EXISTS  author;
@@ -10,10 +10,9 @@ CREATE TABLE author(
 	authorHash CHAR(97) NOT NULL,
 	authorName VARCHAR(32) NOT NULL,
 	authorProfile VARCHAR(220) NOT NULL,
-	--how do I do pictures?
-	authorProfilePicture LONGBLOB NOT NULL,
+	authorProfilePicture LONGBLOB,
 	authorTitle VARCHAR(20),
-	authorTwitterLink ,VARCHAR(32),
+	authorTwitterLink VARCHAR(32),
 	UNIQUE(authorEmail),
 	UNIQUE(authorName),
 	UNIQUE(authorTwitterLink),
@@ -24,9 +23,8 @@ CREATE TABLE article(
 	articleId BINARY(16) NOT NULL,
 	articleAuthorId BINARY(16) NOT NULL,
 	articleCategory VARCHAR(32) NOT NULL,
-	--mySQL documentation says DATETIME requires 8 bytes. why 6?
 	articleDate DATETIME(6) NOT NULL,
-	articleTitle VARCHAR(100)NOT NULL
+	articleTitle VARCHAR(100)NOT NULL,
 	INDEX(articleAuthorId),
 	FOREIGN KEY(articleAuthorId) REFERENCES author(authorId),
 	PRIMARY KEY(articleId)

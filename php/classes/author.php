7b45884467f3c0db5dc2481ec610abc22e8ce931
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-18 11:01:05 -0600

    Initial commit of author class

---
+++ php/classes/author.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author class for ArsTechnica
+ *
+ * This Article Class is an example of data collected and stored about an author for a web publication.
+ *
+ * @author  Jude Baca-Miller <jmiller156@cnm.edu>
+ **/
+
+class Author {
+
+}
+
+?>

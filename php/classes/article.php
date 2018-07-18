6ad4384030ca6a4a4bf284d2b1891f9cefccba18
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-18 16:02:22 -0600

    add accessor and mutator methods for article id

--- php/classes/article.php
+++ php/classes/article.php
@@ -23,3 +23,45 @@ class article {
 	 * @var string $articleCategory
 	 **/
 	private $articleCategory;
+	/**
+	 * date this article was published
+	 * @var \DateTime $articleDate
+	**/
+	private $articleDate;
+	/**
+	 * the title of this article
+	 * @var string $articleTitle
+	 **/
+	private $articleTitle;
+	/**
+	 * the text content of the article.
+	 * @var string $articleContent
+	 */
+	private $aticleContent;
+
+	/**
+	 * accessor method for article id
+	 *
+	 * @return int value of article id
+	 **/
+	public function getAuthorId() : Uuid {
+		return($this->articleId);
+	}
+
+	/**
+	 * mutator method for article id
+	 *
+	 * @param Uuud| sting $newAuthorId value of new article id
+	 * @throws \rangeException if $newAuthorId  is not positive
+	 * @throws \TypeError if article id is not
+	 **/
+	public function  setAuthorId($newAuthorId) : void {
+		try {
+			$uuid = self::validateUuid($newAuthorId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the profile id
+		$this->articleId = $uuid;
+	}

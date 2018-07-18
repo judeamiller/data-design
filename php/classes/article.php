1cdddbea685cac9909e01a40d20229aced6c44a2
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-18 16:57:04 -0600

    add article author id accessor and mutator

--- php/classes/article.php
+++ php/classes/article.php
@@ -44,14 +44,14 @@ class article {
 	 *
 	 * @return int value of article id
 	 **/
-	public function getAuthorId() : Uuid {
+	public function getArticleId() : Uuid {
 		return($this->articleId);
 	}
 
 	/**
 	 * mutator method for article id
 	 *
-	 * @param Uuud| sting $newAuthorId value of new article id
+	 * @param Uuud| sting $newArticleId value of new article id
 	 * @throws \rangeException if $newAuthorId  is not positive
 	 * @throws \TypeError if article id is not
 	 **/
@@ -64,4 +64,32 @@ class article {
 		}
 		// convert and store the profile id
 		$this->articleId = $uuid;
+		}
+	/**
+	 * accessor method for article author id
+	 *
+	 * @return int value of article author id
+	 **/
+	public function getArticleAuthorId() : Uuid {
+		return($this->articleId);
+	}
+
+	/**
+	 * mutator method for article article author id
+	 *
+	 * @param Uuud| sting $newArticleAuthorId value of new article id
+	 * @throws \rangeException if $newArticleAuthorId  is not positive
+	 * @throws \TypeError if article id is not
+	 **/
+	public function  setArticleAuthorId($newArticleAuthorId) : void {
+		try {
+			$uuid = self::validateUuid($newArticleAuthorId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
+		}
+		// convert and store the profile id
+		$this->articleAuthorId = $uuid;
+	}
+
 	}

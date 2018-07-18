4a472eda02951069ed34ce5bf37525e594d3589d
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-18 15:49:30 -0600

    fix typo in author class docblock. Change article class to author class.

--- php/classes/author.php
+++ php/classes/author.php
@@ -2,7 +2,7 @@
 /**
  * Author class for ArsTechnica
  *
- * This Article Class is an example of data collected and stored about an author for a web publication.
+ * This Author Class is an example of data collected and stored about an author for a web publication.
  *
  * @author  Jude Baca-Miller <jmiller156@cnm.edu>
  **/

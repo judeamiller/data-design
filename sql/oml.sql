401377f9c83217bbcf4e21d6a64f67bc7263bc6d
Jude Baca-Miller <judeamiller@gmail.com>
2018-07-19 16:55:26 -0600

    Initial commit of OML file

---
+++ sql/oml.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO author
+(authorId, authorEmail, authorHash, authorName, authorProfile, authorProfilePicture, authorTitle, authorTwitterLink)
+VALUES
+	(UNHEX("07ac95a5ce9f4f17b7552d92ad0de9d4"), "timlee@arstechnica.com", "f1a7ed9c76ee59d16a69c377e1018058262c6c3eaf363ddc1ad16129d19256d345dc29d8d48b3161ad60acdbcd93c5f59", "Tim Lee", "Tim is a writer for Ars.", "http://timothyblee.com/wp-content/uploads/2010/07/timothy_b_lee_portrait.jpg", "Lead Writer", "https://twitter.com/binarybits" );
